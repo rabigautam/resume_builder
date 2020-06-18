0c3efc6cd41109a9fc765e45c6938efde27066cc
rabimatuag <rabigautam43@gmail.com>
2020-06-18 09:14:54 +0545

    configuration

--- server/index.js
+++ server/index.js
@@ -5,16 +5,14 @@
 require('dotenv').config();
 const http = require('http');
 const app = require('./app');
-const port = process.env.PORT || 5250;
-const env = process.env.ENV || 'Development';
-const app_name = process.env.APP_NAME || 'Resume Builder';
+const config = require('./config/index');
 const server = http.createServer(app);
 
-app.set('PORT_NUMBER', port);
+app.set('PORT_NUMBER', config.appPort);
 
 //  Start the app on the specific interface (and port).
-server.listen(port, () => {
-  console.log(`|  ${app_name} started with ${env} Environment on port ${port} at Date ${new Date()}`);
+server.listen(config.appPort, () => {
+  console.log(`|  ${config.appName} started with ${config.appEnv} Environment on port ${config.appPort} at Date ${new Date()}`);
 });
 
 process.on('SIGTERM', () => {
