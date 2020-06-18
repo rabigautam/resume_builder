e0c897e37ecca3c7a8af8ada0433195104a4fbe0
rabimatuag <rabigautam43@gmail.com>
2020-06-18 20:43:16 +0545

    updated

--- server/config/default.js
+++ server/config/default.js
@@ -3,7 +3,7 @@
 /* eslint no-undef: 0 */ // For allowing process.env
 
 module.exports = {
-  appPort: process.env.APIPORT || "5000",
+  appPort: process.env.APIPORT || "3000",
   appName:process.env.APP_Name||"Resume Builder",
   SECRET_KEY: process.env.SECRET_KEY,
   appEnv:process.env.ENV||'Development'
