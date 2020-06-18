23abc6ee06f33fb44d828f5df9007e9d2b171171
rabimatuag <rabigautam43@gmail.com>
2020-06-18 22:16:01 +0545

    updated

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -4,12 +4,12 @@ const router = express.Router();
 const userModule=require('../../modules/user/userController');
 const {authentication}=require('../../helpers/authorization/authMiddleware');
 
-router.post('/', userModule.test);
-router.get('/', userModule.getUsers);
+router.get('/', userModule.test);
+router.get('/',authentication, userModule.getUsers);
 router.post('/register', userModule.register);
 router.post('/login', userModule.login);
 // router.post('/login/google/', passport.authenticate('google-token'), userModule.loginGOath);
-router.post('/login/facebook/',  passport.authenticate('facebook-token'), userModule.loginOauth);
+router.post('/login/facebook',  passport.authenticate('facebook-token'), userModule.loginOauth);
 
 
 module.exports = router;
