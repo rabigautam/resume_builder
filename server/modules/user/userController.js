c1c17e6b7fa343dc7e13c6a1796bda878daf6183
rabimatuag <rabigautam43@gmail.com>
2020-06-18 18:05:16 +0545

    updated with optimize code

--- server/modules/user/userController.js
+++ server/modules/user/userController.js
@@ -1,48 +1,57 @@
-const userSch = require('./userSchema');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('./userConfig');
 const keyConfig = require('../../config/index')
 const otherHelper=require('../../helpers/otherHelper');
 const httpStatus = require('http-status');
+const userSch = require('./userSchema');
 const userController = {};
-userController.test=async (req,res,next)=>{
+userController.test = async (req,res,next)=>{
    try{
-    const user = await userSch.find({});
-       res.json(user);
+       let data=req.body
+       const newResume=  new userSch(data);
+       const saveResume= await newResume.save();
+       return otherHelper.sendResponse(res, httpStatus.OK, true, saveResume, null, 'saved', null);
+
    }catch(err){
        next(err);
    }
 }
 userController.register = async (req, res, next) => {
     try{
-//         const newUser = new userSch({ name, email: email.toLowerCase(),  gender });
-//         const userSave = await newUser.save();
-// console.log(userSave);
         const user = await userSch.findOne({ email: req.body.email.toLowerCase() });
-        const { name,password,gender,password1 } = req.body;
+        let data = req.body;
         if (user) {
           const errors = { email: 'Email already exists' };
           const data = { email: req.body.email };
           return otherHelper.sendResponse(res, httpStatus.CONFLICT, false, data, errors, errors.email, null);
         } else {
-          if (!password1) {
+          if (!data.password1) {
             const err = { password1: 'Field Empty' };
             return otherHelper.sendResponse(res, httpStatus.CONFLICT, false, null, err, 'Field Empty', null);
           }
       
-          if (password != password1) {
+          if (data.password != data.password1) {
             const err = { password1: 'Password Mismatch' };
             return otherHelper.sendResponse(res, httpStatus.CONFLICT, false, null, err, 'Password Mismatch', null);
           }
-          const newUser = new userSch({ name, email: email.toLowerCase(),  gender });
-          bcrypt.genSalt(10, async (err, salt) => {
-            bcrypt.hash(password, salt, async (err, hash) => {
-              if (err) throw err;
-              console.log(hash);
-              newUser.password = hash;
-              const userSave = await newUser.save();
-              const payload = {
+       
+          const generateHash = async (password) =>{
+            try {
+              const saltRound = 10;
+              const salt = await bcrypt.genSalt(saltRound);
+              const hashedPassword = await bcrypt.hash(password, salt);
+              return hashedPassword;
+            } catch (err) {
+              next(err);
+            }
+          };
+         const generated = await generateHash(data.password);
+          data.password=generated;
+          const newUser=  new userSch(data);
+          const userSave= await newUser.save();
+          if(userSave&&userSave._id){
+            const payload = {
                 id: userSave._id,
                 name: userSave.name,
                 email: userSave.email,
@@ -50,63 +59,54 @@ userController.register = async (req, res, next) => {
               };
                // Sign Token
         let token = jwt.sign(payload, keyConfig.SECRET_KEY, {
-            expiresIn: tokenExpireTime,
+            expiresIn: keyConfig.tokenExpireTime,
           });
           token = `Bearer ${token}`;
          
-              return otherHelper.sendResponse(res, httpStatus.OK, true, payload, null, msg, token);
-            });
-          });
+        return otherHelper.sendResponse(res, httpStatus.OK, true, payload, null, 'registered', token);
+        }
+         
+       
         }
     
     }catch(err){
         next(err);
     }
 };
-userController.Login = async (req, res, next) => {
+userController.login = async (req, res, next) => {
     try {
       let errors = {};
-      let email = req.body.email.toLowerCase();
-      const {
-        body: { password },
-      } = req;
-      userSch.findOne({ email }).then((user) => {
-        if (!user) {
-          errors.email = 'User not found';
-          return otherHelper.sendResponse(res, httpStatus.NOT_FOUND, false, null, errors, errors.email, null);
-        }
-  
-        // Check Password
-        bcrypt.compare(password, user.password).then(async (isMatch) => {
-          if (isMatch) {
-       
-         
+      let{password,email}=req.body;
+      console.log(password)
+      const user=await userSch.findOne({email:email.toLowerCase()});
+      if (!user) {
+        errors.email = 'User not found';
+        return otherHelper.sendResponse(res, httpStatus.NOT_FOUND, false, null, errors, errors.email, null);
+      }else{
+        console.log(user.password);
+
+        let isMatch= await  bcrypt.compare(password, user.password);
+        if(isMatch){
             let payload = {
-              id: user._id,
-              name: user.name,
-              email: user.email,
-              email_verified: user.email_verified,
-              gender: user.gender,
-            };
-            // Sign Token
-            jwt.sign(
-              payload,
-              config.SECRET_KEY,
-              {
-                expiresIn: tokenExpireTime,
-              },
-              (err, token) => {
-                token = `Bearer ${token}`;
-              
-                return otherHelper.sendResponse(res, httpStatus.OK, true, payload, null, 'Login Success', token);
-              },
-            );
-          } else {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                // email_verified: user.email_verified,
+                gender: user.gender,
+              };
+        // Sign Token
+        let token = jwt.sign(payload, keyConfig.SECRET_KEY, {
+            expiresIn: keyConfig.tokenExpireTime,
+          });
+          token = `Bearer ${token}`;
+          return otherHelper.sendResponse(res, httpStatus.OK, true, payload, null, 'Login Success', token);
+        }else{
             errors.password = 'Password incorrect';
             return otherHelper.sendResponse(res, httpStatus.BAD_REQUEST, false, null, errors, errors.password, null);
-          }
-        });
-      });
+
+        }
+      }
+    
     } catch (err) {
       next(err);
     }
@@ -154,6 +154,16 @@ userController.Login = async (req, res, next) => {
     token = `Bearer ${token}`;
    
     return otherHelper.sendResponse(res, httpStatus.OK, true, payload, null, 'Register Successfully', token);
+  };
+  userController.getUsers= async (req, res, next) => {
+    try {
+   
+      let userData = await userSch.find();
+      return otherHelper.sendResponse(res, httpStatus.OK, true, userData, null, 'get', null);
+  
+    } catch (err) {
+      next(err);
+    }
   };
   
 module.exports=userController;
